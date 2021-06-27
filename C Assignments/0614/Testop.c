6f60fc66422dd459625c60f7491efdd181ca7db9
sharadaemb <85926628+sharadaemb@users.noreply.github.com>
2021-06-27 21:16:22 +0530

    Add files via upload

---
+++ C Assignments/0614/Testop.c
@@ -0,0 +1,29 @@
+/*Excute Code and observe output*/
+
+#include<stdio.h>
+int main()
+{
+	char ch1,ch2,ch3;
+	ch1=65;
+	ch2='b';
+	ch3='3';
+	printf("ch1=%c\n",ch1);
+	printf("ch2=%c\n",ch2);
+	printf("ch3=%c\n",ch3);
+	printf("ch1=%d\n",ch1);
+	printf("ch2=%d\n",ch2);
+	printf("ch3=%d\n",ch3);
+
+	return 0;
+}
+
+
+/*OUPUT
+ * ch1=A
+ * ch2=b
+ * ch3=3
+ * ch1=65
+ * ch2=98
+ * ch3=51
+ */
+
